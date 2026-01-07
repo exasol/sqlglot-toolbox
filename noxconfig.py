fcd9c4c813af9c0202d2d183b59bcbae0c68e310
Nnamdi Nwabuokei <nwabuokeinnamdi@gmail.com>
2026-01-07 18:17:51 +0100

    chore: implemented assertion rewriting and added docstring

--- noxconfig.py
+++ noxconfig.py
@@ -1,3 +1,9 @@
+"""
+Nox project configuration for the sqlglot-toolbox package.
+
+Defines paths, versioning, and tooling configuration used by nox sessions.
+"""
+
 from __future__ import annotations
 
 from collections.abc import Iterable
@@ -7,9 +13,11 @@ from exasol.toolbox.config import BaseConfig
 
 
 class Config(BaseConfig):
+    """Project configuration for sqlglot-toolbox tooling."""
+
     project_name: str = "sqlglot_toolbox"
     root_path: Path = Path(__file__).parent
-    doc: Path = Path(__file__).parent / "doc"
+    doc: Path = root_path / "doc"
     source: Path = root_path / "exasol" / "sqlglot_toolbox"
     version_file: Path = source / "version.py"
     path_filters: Iterable[str] = ()
