9b8a523a2d7316eff505064730a1b63863798f9e
Nnamdi Nwabuokei <nwabuokeinnamdi@gmail.com>
2026-01-08 16:04:12 +0100

    chore: refactored test removing the default schema and added exasol database version in noxconfig.py

--- test/integration/integration_smoke_test.py
+++ test/integration/integration_smoke_test.py
@@ -2,6 +2,6 @@ import pyexasol
 
 
 def test_number_of_rows_in_my_table(backend_aware_database_params):
-    with pyexasol.connect(**backend_aware_database_params, schema="MY_SCHEMA") as conn:
-        num_of_rows = conn.execute("SELECT COUNT(*) FROM MY_TABLE;").fetchval()
-        assert num_of_rows == 5
+    with pyexasol.connect(**backend_aware_database_params) as conn:
+        num_of_rows = conn.execute("SELECT 1 FROM dual").fetchval()
+        assert num_of_rows >= 1
