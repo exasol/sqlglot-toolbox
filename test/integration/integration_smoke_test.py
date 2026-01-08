4254367bc3577e4ff2af3f9baf0a5226d98046af
Nnamdi Nwabuokei <nwabuokeinnamdi@gmail.com>
2026-01-08 15:29:48 +0100

    chore: add pytest backend

--- test/integration/integration_smoke_test.py
+++ test/integration/integration_smoke_test.py
@@ -1,2 +1,7 @@
-def test_passing_integration_test():
-    assert True
+import pyexasol
+
+
+def test_number_of_rows_in_my_table(backend_aware_database_params):
+    with pyexasol.connect(**backend_aware_database_params, schema="MY_SCHEMA") as conn:
+        num_of_rows = conn.execute("SELECT COUNT(*) FROM MY_TABLE;").fetchval()
+        assert num_of_rows == 5
