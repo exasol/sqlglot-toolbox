506e2073300f98ae0e0c1133f335a48db78f35aa
Ariel Schulz <ariel.schulz@exasol.com>
2026-06-10 09:41:32 +0200

    Use format:fix

--- noxconfig.py
+++ noxconfig.py
@@ -11,8 +11,8 @@ from pathlib import Path
 from exasol.toolbox.config import BaseConfig
 
 PROJECT_CONFIG = BaseConfig(
-    project_name = "sqlglot_toolbox",
-    root_path = Path(__file__).parent,
+    project_name="sqlglot_toolbox",
+    root_path=Path(__file__).parent,
     python_versions=("3.10", "3.11", "3.12", "3.13"),
-    exasol_versions=("2025.1.8",)
+    exasol_versions=("2025.1.8",),
 )
